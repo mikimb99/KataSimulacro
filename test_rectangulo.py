f600a24309bb4811205f622d8d9a633c4058465a
Melero <m.melero1@usp.ceu.es>
2023-05-06 10:19:10 +0200

    Test 1 OK ahora

--- test_rectangulo.py
+++ test_rectangulo.py
@@ -3,10 +3,12 @@ import unittest
 class Test_Rectangulo(unittest.TestCase):
 
     def test_area(self):
-        base= 5
-        altura = 3
-        area = base * altura
-        self.assertEqual((area), 15)
+        x1= 5
+        x2 = 3
+        y1= 5
+        y2= 3
+        area = abs(x1-x2)* abs(y1-y2)
+        self.assertEqual((area), 4)
 
 
 
