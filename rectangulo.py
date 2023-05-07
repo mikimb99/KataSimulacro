9e235bdaf33c614de8137707c8dfc536302534b4
Melero <m.melero1@usp.ceu.es>
2023-05-07 11:54:42 +0200

    Test 4 OK + Refactor de funciones

---
+++ rectangulo.py
@@ -0,0 +1,40 @@
+import math
+
+class Rectangulo:
+    def __init__(self, x1, x2, y1, y2):
+        self.x1=  x1
+        self.x2 = x2
+        self.y1 = y1
+        self.y2 = y2
+
+    def calcular_centro(self):
+        centro_x = (self.x1 + self.x2) / 2
+        centro_y = (self.y1 + self.y2) / 2
+        return (centro_x, centro_y)
+
+    def calcular_perimetro(self):
+        base = abs((self.x2 - self.x1)) or abs(self.y2 - self.y1)
+        altura = abs((self.y1 - self.x1) or (self.y2 - self.x2))
+        if abs(self.y1 - self.x1) == abs(self.y2 - self.x2) and abs(self.x2 - self.x1) == abs(
+                self.y2 - self.y1) and base != altura:
+            return abs(self.x1) + abs(self.x2) + abs(self.y1) + abs(self.y2)
+        else:
+            raise ValueError('No es un rectángulo')
+
+    def calcular_area(self):
+        base = abs((self.x2 - self.x1)) or abs(self.y2 - self.y1)
+        altura = abs((self.y1 - self.x1) or (self.y2 - self.x2))
+        if abs(self.y1 - self.x1) == abs(self.y2 - self.x2) and abs(self.x2 - self.x1) == abs(
+                self.y2 - self.y1) and base != altura:
+            return abs(base) * abs(altura)
+        else:
+            raise ValueError('No es un rectángulo')
+    def calcular_diagonal(self):
+        base = abs((self.x2 - self.x1)) or abs(self.y2 - self.y1)
+        altura = abs((self.y1 - self.x1) or (self.y2 - self.x2))
+        diagonal = math.sqrt(base ** 2 + altura ** 2)
+        # CONDICIONES DE UN RECTÁNGULO
+        if abs(self.y1 - self.x1) == abs(self.y2 - self.x2) and abs(self.x2 - self.x1) == abs(self.y2 - self.y1) and base != altura:
+            return diagonal
+        else:
+            raise ValueError('No es un rectángulo')
--- test_rectangulo.py
+++ test_rectangulo.py
@@ -1,62 +1,22 @@
 import unittest
 import math
+from rectangulo import Rectangulo
 class Test_Rectangulo(unittest.TestCase):
 #X1 X2 Y1 Y2 SON LOS VÉRTICES
     def test_area(self):
-        x1= 0
-        x2= 4
-        y1= -2
-        y2= 2
-        base= abs((x2-x1)) or abs(y2-y1)
-        altura= abs((y1-x1)or (y2-x2))
-        area = abs(base)* abs(altura)
-        # CONDICIONES DE UN RECTÁNGULO
-        if abs(y1-x1)==abs(y2-x2) and abs(x2-x1)==abs(y2-y1) and base!=altura:
-            self.assertEqual((area), 8)
-        else:
-            raise ValueError('No es un rectángulo')
+        rectangulo = Rectangulo(0,4,-2,2)
+        self.assertEqual(rectangulo.calcular_area(), 8)
     def test_perimetro(self):
-        x1 = 0
-        x2 = 4
-        y1 = -2
-        y2 = 2
-        base = abs((x2 - x1)) or abs(y2 - y1)
-        altura = abs((y1 - x1) or (y2 - x2))
-        perimetro= (abs(x1)+abs(x2)+abs(y1)+abs(y2))
-        #CONDICIONES DE UN RECTÁNGULO
-        if abs(y1 - x1) == abs(y2 - x2) and abs(x2 - x1) == abs(y2 - y1) and base != altura:
-            self.assertEqual((perimetro), 8)
-        else:
-            raise ValueError('No es un rectángulo')
+
+        rectangulo = Rectangulo(0, 4, -2, 2)
+        self.assertEqual(rectangulo.calcular_perimetro(), 8)
 
     def test_centro(self):
-        x1 = 0
-        x2 = 4
-        y1 = -2
-        y2 = 2
-        base = abs((x2 - x1)) or abs(y2 - y1)
-        altura = abs((y1 - x1) or (y2 - x2))
-        centrox = (abs(x1) + abs(x2))/2
-        centroy = (abs(y1) + abs(y2))/2
-        centro= (centrox, centroy)
-        # CONDICIONES DE UN RECTÁNGULO
-        if abs(y1 - x1) == abs(y2 - x2) and abs(x2 - x1) == abs(y2 - y1) and base != altura:
-            self.assertEqual((centro), (2.0,2.0))
-        else:
-            raise ValueError('No es un rectángulo')
+        rectangulo = Rectangulo(0, 4, -2, 2)
+        self.assertEqual(rectangulo.calcular_centro(), (2.0, 0.0))
     def test_diagonal(self):
-        x1 = 0
-        x2 = 4
-        y1 = -2
-        y2 = 2
-        base = abs((x2 - x1)) or abs(y2 - y1)
-        altura = abs((y1 - x1) or (y2 - x2))
-        diagonal= math.sqrt(base**2+ altura**2)
-        # CONDICIONES DE UN RECTÁNGULO
-        if abs(y1 - x1) == abs(y2 - x2) and abs(x2 - x1) == abs(y2 - y1) and base != altura:
-            self.assertEqual((diagonal), (4))
-        else:
-            raise ValueError('No es un rectángulo')
+        rectangulo = Rectangulo(0, 4, -2, 2)
+        self.assertEqual(rectangulo.calcular_diagonal(), 4.47213595499958)
 
 
 
