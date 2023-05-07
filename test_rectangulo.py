8c60c664b45c77e87e22d26411ea0ac53ddacef1
Melero <m.melero1@usp.ceu.es>
2023-05-07 11:22:23 +0200

    Test 2

--- test_rectangulo.py
+++ test_rectangulo.py
@@ -1,16 +1,33 @@
 import unittest
 
 class Test_Rectangulo(unittest.TestCase):
-
+#X1 X2 Y1 Y2 SON LOS VÉRTICES
     def test_area(self):
-        x1= 5
-        x2 = 3
-        y1= 5
-        y2= 3
-        area = abs(x1-x2)* abs(y1-y2)
-        self.assertEqual((area), 4)
-
-
+        x1= 0
+        x2= 4
+        y1= -2
+        y2= 2
+        base= abs((x2-x1)) or abs(y2-y1)
+        altura= abs((y1-x1)or (y2-x2))
+        area = abs(base)* abs(altura)
+        # CONDICIONES DE UN RECTÁNGULO
+        if abs(y1-x1)==abs(y2-x2) and abs(x2-x1)==abs(y2-y1) and base!=altura:
+            self.assertEqual((area), 8)
+        else:
+            raise ValueError('No es un rectángulo')
+    def test_perimetro(self):
+        x1 = 0
+        x2 = 4
+        y1 = -2
+        y2 = 2
+        base = abs((x2 - x1)) or abs(y2 - y1)
+        altura = abs((y1 - x1) or (y2 - x2))
+        perimetro= abs(x1+x2+y1+y2)
+        #CONDICIONES DE UN RECTÁNGULO
+        if abs(y1 - x1) == abs(y2 - x2) and abs(x2 - x1) == abs(y2 - y1) and base != altura:
+            self.assertEqual((perimetro), 8)
+        else:
+            raise ValueError('No es un rectángulo')
 
 
 
