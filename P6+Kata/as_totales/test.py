54959af8f54b2e1d72a99649e21d4a3aceeb070a
Melero <m.melero1@usp.ceu.es>
2023-05-16 13:08:50 +0200

    Todo test OK

--- P6+Kata/as_totales/test.py
+++ P6+Kata/as_totales/test.py
@@ -23,5 +23,23 @@ class Testcuenta_a(unittest.TestCase):
             file.write('AA \n')
         count = funcion.count_a(file_path)
         self.assertEqual(count, 2)
+    def testlineas(self):
+        funcion = Funcion()
+        file_path = 'test_file.txt'
+        with open(file_path, 'w') as file:
+            file.write('AA \n')
+            file.write('A       A \n')
+            file.write('A13251346A \n')
+        count = funcion.count_a(file_path)
+        self.assertEqual(count, 6)
+    def testmezcla(self):
+        funcion = Funcion()
+        file_path = 'test_file.txt'
+        with open(file_path, 'w') as file:
+            file.write('Aabonita  \n')
+            file.write('a       eeepaA \n')
+            file.write('a132a51346Aynueveson \n')
+        count = funcion.count_a(file_path)
+        self.assertEqual(count, 9)
 if __name__ == 'main':
     unittest.main()
