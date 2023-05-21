8e74598e1840c0b3d6646f71af6cfbb5afc3c076
Melero <m.melero1@usp.ceu.es>
2023-05-22 00:08:57 +0200

    Fecha hora en el servidor inicial

--- fechahora/funcion.py
+++ fechahora/funcion.py
@@ -11,6 +11,9 @@ class Funcion:
 funcion = Funcion()
 
 # Llamar a los métodos a través de la instancia creada
+
+#Con esto consigo que en el servidor se imprima la fecha y hora actual en el servidor al inicio
+#así puedo comprobar en el servidor que está bien, pero no es necesario
 fecha_actual = funcion.obtener_fecha()
 hora_actual = funcion.obtener_hora()
 
